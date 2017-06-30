48cafd73da28a2bdf8f41a14e9b0f49e042369a3
EasternCatNZL <brianpwwong@gmail.com>
2017-06-30 12:20:05 +1200

    Got object names in position report

--- UdemyEscape/Source/UdemyEscape/PositionReport.cpp
+++ UdemyEscape/Source/UdemyEscape/PositionReport.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "PositionReport.h"
-
+#include "Gameframework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReport::UPositionReport()
@@ -20,7 +20,9 @@ void UPositionReport::BeginPlay()
 	Super::BeginPlay();
 
 	// ...
-	UE_LOG(LogTemp, Warning, TEXT("Position report reporting for duty!"));
+	FString objectName = GetOwner()->GetName();
+
+	UE_LOG(LogTemp, Warning, TEXT("Position report for %s"), *objectName);
 }
 
 
